7f04a3cc78b83799155717e6e2eecc2eddcd7957
amartin58 <adee1001@outlook.com>
2021-08-25 07:30:50 -0700

    Add Event Listeners

--- adapp.js
+++ adapp.js
@@ -11,6 +11,7 @@ function getTitle() {
   xhr.onload = function () {
     if (xhr.status == 200) {
       const webpageTitle = JSON.parse(xhr.response);
+
       let output = `${webpageTitle.welcome}`;
       document.getElementsByClassName("title")[0].innerHTML = output;
     } else {
@@ -43,13 +44,17 @@ function getOptions() {
   xhr.onload = function () {
     if (xhr.status == 200) {
       const transOptions = JSON.parse(xhr.response);
+      let selectTransaction = transOptions.selectTransactionMessage;
+      let startTransaction = transOptions.startTransaction;
+      let currentTransaction = transOptions.currentTransaction;
+      let exitTransactions = transOptions.exitTransaction;
+
       let output = "";
-      output += `<h3>${transOptions.selectTransactionMessage}</h3>
-      <ul>
-        <li>${transOptions.startTransaction}</li>
-        <li>${transOptions.currentTransaction}</li>
-        <li>${transOptions.exitTransaction}</li>
-        </ul>`;
+      output += `<h3>${selectTransaction}</h3>
+      <h5>${startTransaction}</h5>
+      <h5>${currentTransaction}</h5>
+      <h5>${exitTransactions}</h5>
+        `;
       document.getElementsByClassName("options")[0].innerHTML = output;
     } else {
       document.getElementsByClassName("options")[0].innerHTML = "Error";
@@ -59,61 +64,110 @@ function getOptions() {
   xhr.send();
 }
 
+
 function getFood() {
   let xhr = new XMLHttpRequest();
   xhr.onload = function () {
     if (xhr.status == 200) {
       const foodOptions = JSON.parse(xhr.response);
+      optionsMessage = foodOptions.foodOptionSelectMsg;
+      food1 = foodOptions.foodOption1;
+      food2 = foodOptions.foodOption2;
+      food3 = foodOptions.foodOption3;
+      food4 = foodOptions.foodOption4;
+
       let output = "";
-      output += `<h3>${foodOptions.foodOptionSelectMsg}</h3>
-      <ul>
-      <li>${foodOptions.foodOption1}</li><button class="button1">HI there</button>
-      <li>${foodOptions.foodOption2}</li> 
-      <li>${foodOptions.foodOption3}</li> 
-      <li>${foodOptions.foodOption4}</li>
-        </ul>  
-        <h4>${foodOptions.foodAddedMsg}</h4>`;
-      document.getElementsByClassName("food")[0].innerHTML = output;
+      output += `     
+  <h3>${optionsMessage}</h3>
+   <p>
+  <label for="food1"> <input type="checkbox" name="food" value="${food1}" id="food1" />${food1}</label>
+  </p>
+  <p>
+  <label for="food2"> <input type="checkbox" name="food" value="${food2}" id="food2" />${food2}</label>
+  </p>
+  <p>
+  <label for="food3"> <input type="checkbox" name="food" value="${food3}" id="food3" />${food3}</label>
+  </p>  
+  <p>
+  <label for="food4"> <input type="checkbox" name="food" value="${food4}" id="food4" />${food4}</label>
+  </p>     
+        `;
+
+      document.getElementById("food").innerHTML = output;
     } else {
-      document.getElementsByClassName("food")[0].innerHTML = "Error";
+      document.getElementsByClassName("food").innerHTML = "Error";
     }
+
+      document.getElementById("food1").addEventListener("click", function () {
+        document.getElementById(
+          "message1"
+        ).innerHTML = `You added a ${food1.slice(5, 12)}  
+          `;
+      });
+
+    document.getElementById("food2").addEventListener("click", function () {
+      document.getElementById(
+        "message1"
+      ).innerHTML = `You added a ${food2.slice(5, 12)}  
+          `;
+    });
+
+    document.getElementById("food3").addEventListener("click", function () {
+      document.getElementById("message1").innerHTML = `You added ${food3.slice(
+        5,
+        12
+      )}
+          `;
+    });
   };
   xhr.open("GET", "http://localhost:3000/food", true);
   xhr.send();
 }
-
-document.getElementById("button1").addEventListener("click", function (e) {
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://localhost:3000/food");
-  xhr.setRequestHeader("Content-Type", "application/json");
-  xhr.onload = function () {
-    if (xhr.status == 200) {
-      document.getElementById("message").innerHTML = xhr.response;
-    }
-  };
-  xhr.send(JSON.stringify({ food: "test", price: "7.00" }));
-});
 
 function getPayment() {
   let xhr = new XMLHttpRequest();
   xhr.onload = function () {
     if (xhr.status == 200) {
       const payOptions = JSON.parse(xhr.response);
+      let paySection = payOptions.paymentSectionMsg;
+      let startPayment = payOptions.startPayMsg;
+      let paymentOption = payOptions.startPayMethod;
+      let totalOfTransactions = payOptions.paymentTotal;
+      let payByCash = payOptions.paymentCash;
+      let payByCredit = payOptions.paymentCredit;
+      let paymentMethod = payOptions.paymentChoiceSelection;
+      let paymentThanks = payOptions.paymentSelectionMsg;
+
       let output = "";
-      output += `<h3>${payOptions.paymentSectionMsg}</h3><br><br>
-      <h4>${payOptions.startPayMsg}</h4><br>
-      <h3>${payOptions.paymentTotal}</h3>
-      <h3>${payOptions.startPayMethod}</h3><br>
-      <ul>
-      <h4>${payOptions.paymentCash}</h4>
-      <h4>${payOptions.paymentCredit}</h4><br>
-      <h4>${payOptions.paymentChoiceSelection}</h4>
-      <h4>${payOptions.paymentSelectionMsg}</h4>
-      </ul>`;
+      output += `<h3>${paySection}</h3><br><br>
+      <h4>${startPayment}</h4><br>
+      <h3>${totalOfTransactions}</h3>
+      <h3>${paymentOption}</h3><br>
+
+      <p>
+      <label for="cash-payment"><input type="checkbox" name="payment" value="${payByCash}" id="cash-payment" />${payByCash}</label>
+      </p>
+      <p>
+      <label for="credit-payment"><input type="checkbox" name="payment" value="${payByCredit}" id="credit-payment" />${payByCredit}</label>
+      </p>
+      <h4>${paymentMethod}</h4>
+      <h4>${paymentThanks}</h4>
+      
+            `;
       document.getElementsByClassName("payment")[0].innerHTML = output;
     } else {
       document.getElementsByClassName("payment")[0].innerHTML = "Error";
     }
+
+    document.getElementById("cash-payment").addEventListener("click", function() {
+      document.getElementById("message2").innerHTML = ``;
+      
+    });
+
+    document.getElementById("cash-payment").addEventListener("click", function() {
+      document.getElementById("message2").innerHTML = `${payByCredit}`;
+    });
+
   };
   xhr.open("GET", "http://localhost:3000/payment", true);
   xhr.send();
@@ -160,4 +214,3 @@ function getEndingTransactions() {
   xhr.open("GET", "http://localhost:3000/endingTransactions", true);
   xhr.send();
 }
-
